76bf5b69f96c42be99156554d37c043c6776f047
SohailAman <101714964+SohailAman@users.noreply.github.com>
2023-02-28 21:47:07 +0500

    Smooth scroll

--- js/script.js
+++ js/script.js
@@ -120,3 +120,13 @@ window.onscroll = () => {
 
     }
 }
+$(document).ready(function () {
+    // Show or hide the back-to-top button
+
+    // Smooth scroll to top
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+});
+
